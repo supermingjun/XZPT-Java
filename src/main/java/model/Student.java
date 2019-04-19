904ab91ab4767e3368aac3855c6b1af70324334c
murphy-li <murphy-li@foxmail.com>
2019-04-19 13:41:42 +0800

    添加配置文件，添加Student实体

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,131 @@
+package model;
+
+/**
+ * @author Murphy
+ * @date 2019/4/19 13:21
+ */
+public class Student {
+
+    private String studentId;
+    private String passwd;
+    private String telephone;
+    private String userName;
+    private String headUrl;
+    private String email;
+    private int sex;
+    private String school;
+    private String specialty;
+    private String occupation;
+    private String presentCity;
+    private String expectedCity;
+
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+    public String getHeadUrl() {
+        return headUrl;
+    }
+
+    public void setHeadUrl(String headUrl) {
+        this.headUrl = headUrl;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+
+    public String getPresentCity() {
+        return presentCity;
+    }
+
+    public void setPresentCity(String presentCity) {
+        this.presentCity = presentCity;
+    }
+
+
+    public String getExpectedCity() {
+        return expectedCity;
+    }
+
+    public void setExpectedCity(String expectedCity) {
+        this.expectedCity = expectedCity;
+    }
+
+}
+
