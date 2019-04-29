de1a8bc1e6632c48f5fd5ee9700229533a1929e9
litm <975835798@qq.com>
2019-04-29 21:09:59 +0800

    算法修正

---
+++ src/main/java/xz/fzu/algorithm/EnterpriseSimilarityResult.java
@@ -0,0 +1,33 @@
+package xz.fzu.algorithm;
+
+/**
+ * 该类用来保存企业id以及相似性结果
+ * @author LITM
+ *
+ */
+public class EnterpriseSimilarityResult implements Comparable<EnterpriseSimilarityResult>{
+	
+	private String recruitmentId;
+	private double SimilarityResult;
+	public String getRecruitmentId() {
+		return recruitmentId;
+	}
+	public void setRecruitmentId(String recruitmentId) {
+		
+		this.recruitmentId = recruitmentId;
+	}
+	public double getSimilarityResult() {
+		
+		return SimilarityResult;
+	}
+	public void setSimilarityResult(double similarityResult) {
+		
+		SimilarityResult = similarityResult;
+	}
+	@Override
+	public int compareTo(EnterpriseSimilarityResult o) {
+		
+		return o.getSimilarityResult()>this.getSimilarityResult()?0:-1;
+	}
+	
+}
