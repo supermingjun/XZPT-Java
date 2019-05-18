c978f84f822b1238e8ce41ae4b43a66de0bb8256
murphy-li <murphy-li@foxmail.com>
2019-05-18 21:25:06 +0800

    更新上传文件代码

--- src/main/java/xz/fzu/controller/UploadController.java
+++ src/main/java/xz/fzu/controller/UploadController.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import xz.fzu.exception.TokenExpiredException;
+import xz.fzu.service.IUserService;
 import xz.fzu.vo.ResponseVO;
 
+import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
@@ -22,6 +25,18 @@ import java.util.UUID;
 @RestController
 @RequestMapping(value = "/upload", method = RequestMethod.POST)
 public class UploadController {
+
+    private static final String APP_HOME = "/var/lib/tomcat8/webapps/XZPT-Java-1.0-SNAPSHOT/file/";
+    /**
+     * 上传文件接口
+     *
+     * @param file 文件
+     * @return xz.fzu.vo.ResponseVO<java.lang.String>
+     * @author Murphy
+     * @date 2019/5/2 21:43
+     */
+    @Resource
+    IUserService iUserService;
 
     /**
      * 上传文件接口
@@ -43,7 +58,7 @@ public class UploadController {
             format = "jpg";
         }
         String fileName = UUID.randomUUID() + "." + format;
-        String path = "../webapps/img/" + UUID.randomUUID() + "." + fileName;
+        String path = APP_HOME + "image/" + UUID.randomUUID() + "." + fileName;
 
         File newFile = new File(path);
 
@@ -54,18 +69,11 @@ public class UploadController {
         return responseVO;
     }
 
-    /**
-     * 上传文件接口
-     *
-     * @param file 文件
-     * @return xz.fzu.vo.ResponseVO<java.lang.String>
-     * @author Murphy
-     * @date 2019/5/2 21:43
-     */
     @RequestMapping("/file")
-    public ResponseVO<String> fileUpload(@RequestParam("file") CommonsMultipartFile file) throws IOException {
+    public ResponseVO<String> fileUpload(@RequestParam("file") CommonsMultipartFile file, @RequestParam String token) throws IOException, TokenExpiredException {
 
         ResponseVO<String> responseVO = new ResponseVO<>();
+        String userId = iUserService.selectUserByToken(token).getUserId();
         String[] names = Objects.requireNonNull(file.getOriginalFilename()).split("\\.");
         String format;
         if (names.length - 1 >= 0) {
@@ -74,14 +82,15 @@ public class UploadController {
             format = "csv";
         }
         String fileName = UUID.randomUUID() + "." + format;
-        String path = "../webapps/csv/" + UUID.randomUUID() + "." + fileName;
+        String path = APP_HOME + userId +
+                "/upload/" +
+                UUID.randomUUID() + "." + fileName;
 
         File newFile = new File(path);
 
         //通过CommonsMultipartFile的方法直接写文件（注意这个时候）
         file.transferTo(newFile);
         responseVO.setResultObject(fileName);
-
         return responseVO;
     }
 }
