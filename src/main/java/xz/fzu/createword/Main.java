e02ac2cb5acf8beae688ce01100382dbc35aa901
litm <975835798@qq.com>
2019-05-25 14:05:42 +0800

    Revert "修改函数"
    This reverts commit 90abf13a

--- src/main/java/xz/fzu/createword/Main.java
+++ src/main/java/xz/fzu/createword/Main.java
@@ -1,19 +1,12 @@
 package xz.fzu.createword;
 
-import xz.fzu.model.Resume;
-
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Resume resume = new Resume();
-		resume.setCertificate("sfdsd");
-		resume.setEmail("sdfsdf");
-		resume.setHeadUrl("E:\\java代码\\简历生成Word\\src\\image.jpg");
-		resume.setHighestEducation((long)3);
-		resume.setPracticalExperience("sdfsdf");
-		resume.setSchool("fzu");
-		resume.setSpeciality("software");
-		resume.setTelephone("1111111");
-		resume.setUserName("liyanwen");
-		CreateWord.createWord(resume,"E:\\java代码\\生成pdf\\src\\ceshi\\jianli2.docx","jianli.ftl","E:\\gitworkspace\\XZPT-Java\\src\\main\\resources\\resume_template");
+
+
+
+//	     String imageBase64 = LoadImage.loadImage("E:\\ps材料\\8.14\\沙沙-PS打造帅气纹身效果\\沙沙-PS打造帅气纹身效果\\ps打造帅气纹身效果素材\\35373.jpg");
+	   //map.put("head_sculpture",imageBase64);
+//	     CreateWord.createWord(map,"E:\\java代码\\生成pdf\\src\\ceshi\\jianli2.docx","jianli.ftl","E:\\java代码\\简历生成Word\\src");
 	}
 }
